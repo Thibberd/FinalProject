22547a79607e834486432117d8518d12694a1b3b
Thomas Hibberd <thomas.hibberd@students.plymouth.ac.uk>
2018-04-27 02:45:50 +0100

    Management System

---
+++ ManagementApplication/ManagementApplication/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagementApplication
+{
+    class User
+    {
+        public int userID { get; set; }
+        public int userType { get; set; }
+        public String firstname { get; set; }
+        public String secondname { get; set; }
+        public int PIN { get; set; }
+    }
+}
